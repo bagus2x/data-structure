43eda0fa294e031165d6e5cd8e2b230e9981ee41
bagus2x <dutapanggulu@gmail.com>
2020-10-18 01:27:18 +0700

    fix bug on removeLast()

--- stack/Stack.java
+++ stack/Stack.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.EmptyStackException;
 
 class Stack<T> {
@@ -21,9 +20,9 @@ class Stack<T> {
 
     public void push(T data) {
         Node<T> node = new Node<T>(data);
-        if(size == 0) {
+        if (size == 0) {
             head = tail = node;
-        }else {
+        } else {
             tail.next = node;
             tail = tail.next;
         }
@@ -31,58 +30,62 @@ class Stack<T> {
     }
 
     @SafeVarargs
-    final public void push(T ...data) {
-        for(T d: data) {
+    final public void push(T... data) {
+        for (T d : data) {
             push(d);
         }
     }
 
     public void unshift(T data) {
-        if(size == 0) {
+        if (size == 0) {
             head = tail = new Node<T>(data);
-        }else {
+        } else {
             head = new Node<>(data, head);
         }
         size++;
     }
 
     @SafeVarargs
-    final public void unshift(T ...data) {
-        for(int i = data.length - 1; i >= 0; i--) {
+    final public void unshift(T... data) {
+        for (int i = data.length - 1; i >= 0; i--) {
             unshift(data[i]);
         }
     }
 
     public T shift() {
-        if(size == 0 ) new EmptyStackException();
+        if (size == 0)
+            new EmptyStackException();
         T shiftData = head.data;
         removeFirst();
         return shiftData;
     }
 
     public void removeFirst() {
-        if(size == 0 ) new EmptyStackException();
+        if (size == 0)
+            new EmptyStackException();
         head = head.next;
         size--;
     }
 
     public T pop() {
-        if(size == 0) throw new EmptyStackException();
+        if (size == 0)
+            throw new EmptyStackException();
         T popData = tail.data;
         removeLast();
         return popData;
     }
 
     public void removeLast() {
-        if(size == 0) throw new EmptyStackException();
-        if(size == 1) {
+        if(head == null) throw new EmptyStackException();
+        if(head.next == null) {
             head = tail = null;
         }else {
-            Node<T> current = head;
+            Node<T> current =  head;
             while(current.next.next != null) {
-                current = current.next;
+                current =  current.next;
             }
             current.next = null;
+            tail = current;
         }
         size--;
     }
@@ -91,10 +94,10 @@ class Stack<T> {
     public String toString() {
         String res = "[";
         Node<T> current = head;
-        while(current != null) {
+        while (current != null) {
             res += current.data;
             current = current.next;
-            if(current != null) {
+            if (current != null) {
                 res += ", ";
             }
         }
